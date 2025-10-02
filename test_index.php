54876f284c944080091dcdf7c46f78ccb184b510
Tracey Lartey <122820133+Kuku289@users.noreply.github.com>
2025-10-02 14:12:48 +0700

    Complete Part 1 & 2: Authentication System and Category Management

--- test_index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'controllers/user_controller.php';
-$email = "admin.test@gmail.com";
-$exists = check_email_exists_ctr($email);
-echo "Email '$email' exists: " . ($exists ? "YES" : "NO");
-?>
