de7d409c99e1bb82cbf8b296b0a5486f08fb6af7
Tracey Lartey <122820133+Kuku289@users.noreply.github.com>
2025-10-26 14:27:26 +0700

    Add complete Brand Management system (Part 1)

---
+++ controllers/brand_controller.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Brand Controller - Business logic layer for brands
+ * File: controllers/brand_controller.php
+ */
+
+require_once '../classes/brand_class.php';
+
+/**
+ * Add a new brand
+ * @param string $brand_name - Brand name
+ * @param int $cat_id - Category ID
+ * @param int $user_id - User ID
+ * @return int|false - Brand ID on success, false on failure
+ */
+function add_brand_ctr($brand_name, $cat_id, $user_id)
+{
+    if (empty($brand_name) || empty($cat_id) || empty($user_id)) {
+        error_log("add_brand_ctr: Empty parameters");
+        return false;
+    }
+    
+    $brand_name = trim($brand_name);
+    if (strlen($brand_name) < 2 || strlen($brand_name) > 100) {
+        error_log("add_brand_ctr: Invalid brand name length");
+        return false;
+    }
+    
+    $brand = new Brand();
+    $result = $brand->addBrand($brand_name, $cat_id, $user_id);
+    
+    if ($result) {
+        error_log("add_brand_ctr: Successfully added brand with ID: " . $result);
+    } else {
+        error_log("add_brand_ctr: Failed to add brand");
+    }
+    
+    return $result;
+}
+
+/**
+ * Get all brands for a user
+ * @param int $user_id - User ID
+ * @return array|false - Array of brands or false
+ */
+function get_brands_by_user_ctr($user_id)
+{
+    if (empty($user_id)) {
+        error_log("get_brands_by_user_ctr: Empty user_id");
+        return false;
+    }
+    
+    $brand = new Brand();
+    return $brand->getBrandsByUser($user_id);
+}
+
+/**
+ * Get a single brand by ID
+ * @param int $brand_id - Brand ID
+ * @param int $user_id - User ID
+ * @return array|false - Brand data or false
+ */
+function get_brand_by_id_ctr($brand_id, $user_id)
+{
+    if (empty($brand_id) || empty($user_id)) {
+        error_log("get_brand_by_id_ctr: Empty parameters");
+        return false;
+    }
+    
+    $brand = new Brand();
+    return $brand->getBrandById($brand_id, $user_id);
+}
+
+/**
+ * Update a brand
+ * @param int $brand_id - Brand ID
+ * @param string $brand_name - New brand name
+ * @param int $user_id - User ID
+ * @return bool - True on success, false on failure
+ */
+function update_brand_ctr($brand_id, $brand_name, $user_id)
+{
+    if (empty($brand_id) || empty($brand_name) || empty($user_id)) {
+        error_log("update_brand_ctr: Empty parameters");
+        return false;
+    }
+    
+    $brand_name = trim($brand_name);
+    if (strlen($brand_name) < 2 || strlen($brand_name) > 100) {
+        error_log("update_brand_ctr: Invalid brand name length");
+        return false;
+    }
+    
+    $brand = new Brand();
+    return $brand->updateBrand($brand_id, $brand_name, $user_id);
+}
+
+/**
+ * Delete a brand
+ * @param int $brand_id - Brand ID
+ * @param int $user_id - User ID
+ * @return bool - True on success, false on failure
+ */
+function delete_brand_ctr($brand_id, $user_id)
+{
+    if (empty($brand_id) || empty($user_id)) {
+        error_log("delete_brand_ctr: Empty parameters");
+        return false;
+    }
+    
+    $brand = new Brand();
+    return $brand->deleteBrand($brand_id, $user_id);
+}
+
+/**
+ * Check if a brand + category combination exists
+ * @param string $brand_name - Brand name
+ * @param int $cat_id - Category ID
+ * @param int $user_id - User ID
+ * @return bool - True if exists, false otherwise
+ */
+function check_brand_category_exists_ctr($brand_name, $cat_id, $user_id)
+{
+    if (empty($brand_name) || empty($cat_id) || empty($user_id)) {
+        error_log("check_brand_category_exists_ctr: Empty parameters");
+        return false;
+    }
+    
+    $brand = new Brand();
+    return $brand->brandCategoryExists($brand_name, $cat_id, $user_id);
+}
+?>
